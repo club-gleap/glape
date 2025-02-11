442d98f77d5ddf3e121c1dadf52aefeed5ddf82c
Ytampo <angerball8@gmail.com>
2025-02-11 18:38:44 +0900

    update: discordでログイン(W.I.P. , 404エラー)

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,12 +1,13 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-export default NextAuth({
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET,
-        }),
-    ],
-    // 他の設定...
-});
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
