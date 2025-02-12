19cedc1603749367dc7d39f1a2bcf4f67e6f49c3
tsukuru296 <tututuku.fufufu296@gmail.com>
2025-02-12 18:51:04 +0900

    feature/2-loginpage

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,25 @@
-import { div } from "framer-motion/client";
-
+"use client"
+import { Box,Button,Center, Flex, FormControl, Input } from "@chakra-ui/react";
+import { EmailIcon,LockIcon } from "@chakra-ui/icons";
 export default function LoginPage(){
 	return (
-	<p>"こんにちは"</p>
-	<button>Login</button>;
+		<Center h="100vh" flexDirection="column">
+			<Box fontSize="30px">メンバーログイン</Box>
+			<Box>Hello World</Box>
+			<Flex width={["80%","50%"]} mt="20px">
+				<EmailIcon boxSize="40px" mr="10px"></EmailIcon>
+				<FormControl>
+					<Input type="email"></Input>
+				</FormControl>
+			</Flex>
+			<Flex width={["80%","50%"]} mt="20px">
+				<LockIcon boxSize="40px" mr="10px"></LockIcon>
+				<FormControl>
+					<Input type="password"></Input>
+				</FormControl>
+			</Flex>
+			<Button mt="50px">アカウント登録はこちら</Button>
+		</Center>
+
 	)
 }
