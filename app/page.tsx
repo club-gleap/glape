f38a621efc5019e3840f616391d4b57c5f1d76d4
Ytampo <angerball8@gmail.com>
2025-02-13 00:15:23 +0900

    update: css修正

--- app/page.tsx
+++ app/page.tsx
@@ -15,21 +15,21 @@ export default function Page() {
 				direction={{base: 'column', lg:'row'}} 
 				gap={"0"}
 			>
-				<VStack h={{base:"", lg:"100%"}} w={{base:"100%", lg:"50%"}} p={["3", "6"]} justifyContent={"center"}>
+				<VStack h={{base:"", lg:"100%"}} w={{base:"100%", lg:"50%"}} p={{base:"12px", md:"16px", lg:"20px"}} justifyContent={"center"}>
 					<Image src="img/glape-icon.png" w={{base:"40%", lg:"75%"}} rounded={"15%"} alt=""></Image>
-					<Image src="img/glape-logo.svg" display={["block", "none"]} w={"60%"} alt=""></Image>
+					{/* <Image src="img/glape-logo.svg" display={["block", "none"]} w={"60%"} alt=""></Image> */}
 				</VStack>
-				<Flex h={{base:"", lg:"100%"}} w={{base: "100%", lg: "50%"}} minWidth={["0", "550px"]} p={["3", "6"]}>
-					<VStack h="100%" w="100%" p={["3", "8"]} bg="gray.100" rounded="0.5rem" gap="1.5rem" alignItems={{base:"center", lg:"flex-start"}} justifyContent={"center"}>
-						<Image src="img/glape-logo.svg" display={["none", "block"]} w={"300px"} alt=""></Image>
-						<Text fontSize={["4xl", "5xl","6xl"]} fontWeight={"bold"} color={"gray.700"}>サークルイベントを<br />これ１つで。</Text>
+				<Flex h={{base:"", lg:"100%"}} w={{base: "100%", lg: "50%"}} minWidth={{base:"0", lg:"550px"}} p={{base:"12px", md:"16px", lg:"20px"}}>
+					<VStack h="100%" w="100%" p={{base:"12px", md:"16px", lg:"20px"}} bg="gray.100" rounded="0.5rem" gap="1.5rem" alignItems={{base:"center", lg:"flex-start"}} justifyContent={"center"}>
+						<Image src="img/glape-logo.svg" w={{base:"70%",md:"50%" ,lg:"300px"}} alt=""></Image>
+						<Text fontSize={{base:"4xl", md:"5xl", lg:"6xl"}} fontWeight={"bold"} color={"gray.700"}>サークルイベントを<br />これ１つで。</Text>
 						<form
 							action={discordLogin}
 						>
-						<Button type="submit" w={{base:"18em", lg:"20em"}} minHeight="20px" size={"lg"} leftIcon={<FaDiscord />} colorScheme={"pink"}>Discordでログイン・登録</Button>
+						<Button type="submit" w={{base:"15em", md:"18em", lg:"20em"}} minHeight="20px" size={"lg"} leftIcon={<FaDiscord />} colorScheme={"pink"}>Discordでログイン・登録</Button>
 						</form>
-						<Button w={{base:"18em", lg:"20em"}} minHeight="20px" size={"lg"} leftIcon={<FaEnvelope />} colorScheme={"blue"}>メールアドレスでログイン</Button>
-						<Button w={{base:"18em", lg:"20em"}} minHeight="20px" size={"lg"} leftIcon={<FaCheck />} colorScheme={"green"}>新規登録</Button>
+						<Button w={{base:"15em", md:"18em", lg:"20em"}} minHeight="20px" size={"lg"} leftIcon={<FaEnvelope />} colorScheme={"blue"}>メールアドレスでログイン</Button>
+						<Button w={{base:"15em", md:"18em", lg:"20em"}} minHeight="20px" size={"lg"} leftIcon={<FaCheck />} colorScheme={"green"}>新規登録</Button>
 						<HStack>
 							<Text>Powered By</Text>
 							<Link href="https://circle.gleap.tech/" target="_blank" rel="noopener noreferrer"><u>GLEAP</u></Link>
