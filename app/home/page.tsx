0d25eafa2394d6a94e25858fcf18f9409f112909
tanimoto <98684774+Ytampo@users.noreply.github.com>
2025-02-14 11:39:55 +0900

    update: メアドログインへの遷移

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,27 +1,13 @@
 'use client';
 
-import { Box, Stack, Center, Text, Flex, Image, VStack, Button, HStack } from "@chakra-ui/react";
-import Link from "next/link";
-// icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-// auth
-import { signIn, useSession } from "next-auth/react";
-
+import { Box, Stack, Center, } from "@chakra-ui/react";
+// import Link from "next/link";
 
 export default function Page() {
 	return (
 		<Box h="100vh" w="100vw" bg="white">
 			<Stack h="100%" w="100%" direction={['column', 'row']} gap={"0"}>
-				<VStack h={["", "100%"]} w={["100%", "50%"]} p={["3", "6"]} justifyContent={"center"}>
-					<Image src="img/glape-icon.png" w={["40%", "75%"]} rounded={"15%"}></Image>
-					<Image src="img/glape-logo.svg" display={["block", "none"]} w={"60%"}></Image>
-				</VStack>
-				<Center h={["", "100%"]} w={["100%", "50%"]} p={["3", "6"]}>
-          <Text>ログインに成功しました。</Text>
-				</Center>
+				<Center h="100%" w="100%">home</Center>
 			</Stack>
 		</Box>
 	)
