f1f4800769eaf2218c3b8b367ee96a821dcaa78a
tanimoto <98684774+Ytampo@users.noreply.github.com>
2025-02-14 12:12:39 +0900

    update: コメントアウトの削除

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Page() {
 			>
 				<VStack h={{base:"", lg:"100%"}} w={{base:"100%", lg:"50%"}} p={{base:"12px", md:"16px", lg:"20px"}} justifyContent={"center"}>
 					<Image src="img/glape-icon.png" w={{base:"40%", lg:"75%"}} rounded={"15%"} alt=""></Image>
-					{/* <Image src="img/glape-logo.svg" display={["block", "none"]} w={"60%"} alt=""></Image> */}
 				</VStack>
 				<Flex h={{base:"", lg:"100%"}} w={{base: "100%", lg: "50%"}} minWidth={{base:"0", lg:"550px"}} p={{base:"12px", md:"16px", lg:"20px"}}>
 					<VStack h="100%" w="100%" p={{base:"12px", md:"16px", lg:"20px"}} bg="gray.100" rounded="0.5rem" gap="1.5rem" alignItems={{base:"center", lg:"flex-start"}} justifyContent={"center"}>
