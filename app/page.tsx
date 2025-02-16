775f43461ae30b2c77559e57755f74dc0fbc332b
Ytampo <angerball8@gmail.com>
2025-02-16 22:21:14 +0900

    update: Centerに変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Stack, Text, Flex, Image, VStack, Button, HStack } from "@chakra-ui/react";
+import { Box, Stack, Text, Flex, Image, VStack, Button, HStack, Center } from "@chakra-ui/react";
 import Link from "next/link";
 import discordLogin from "./serverAction";
 // icons
@@ -15,9 +15,9 @@ export default function Page() {
 				direction={{base: 'column', lg:'row'}} 
 				gap={"0"}
 			>
-				<VStack h={{base:"", lg:"100%"}} w={{base:"100%", lg:"50%"}} p={{base:"12px", md:"16px", lg:"20px"}} justifyContent={"center"}>
+				<Center h={{base:"", lg:"100%"}} w={{base:"100%", lg:"50%"}} p={{base:"12px", md:"16px", lg:"20px"}} justifyContent={"center"}>
 					<Image src="img/glape-icon.png" w={{base:"40%", lg:"75%"}} rounded={"15%"} alt=""></Image>
-				</VStack>
+				</Center>
 				<Flex h={{base:"", lg:"100%"}} w={{base: "100%", lg: "50%"}} minWidth={{base:"0", lg:"550px"}} p={{base:"12px", md:"16px", lg:"20px"}}>
 					<VStack h="100%" w="100%" p={{base:"12px", md:"16px", lg:"20px"}} bg="gray.100" rounded="0.5rem" gap="1.5rem" alignItems={{base:"center", lg:"flex-start"}} justifyContent={"center"}>
 						<Image src="img/glape-logo.svg" w={{base:"70%",md:"50%" ,lg:"300px"}} alt=""></Image>
