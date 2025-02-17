897256438f4ed1b9add82a28b06af4bb4de137ed
tsukuru296 <tututuku.fufufu296@gmail.com>
2025-02-17 09:20:56 +0900

    イベント追加画面を作成した

--- app/event/add/page.tsx
+++ app/event/add/page.tsx
@@ -1,14 +1,14 @@
 "use client"
-import { Box,Button,Center,Input, Stack, Text} from "@chakra-ui/react";
+import { Box,Button,Center,Input, Stack} from "@chakra-ui/react";
 export default function eventAdd(){
 	return (
-        <Center flexDirection={"column"} h="100vh" w="100vw">
-            <Stack direction="column" w={["80%","50%"]} gap="10">
+        <Center h="100vh" w="100vw">
+            <Stack w={["80%","50%"]} gap="10">
                 <Box fontSize="30px" textAlign="center">イベントを作成する</Box>
                 <Input placeholder='イベント名' />
                 <Input type="date" />
                 <Input placeholder='公開範囲' />
-                <Button size="lg" colorScheme="teal">作成</Button>
+                <Button size="lg" colorScheme="purple">作成</Button>
             </Stack>
         </Center>
     )
