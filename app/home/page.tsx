0be86739ee0ff4020d5d938cabad3cb6654bc8d2
tanimoto <98684774+Ytampo@users.noreply.github.com>
2025-02-17 11:29:17 +0900

    fix: PR修正2(prettierで整形)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,16 @@
-'use client';
+"use client";
 
-import { Box, Stack, Center, } from "@chakra-ui/react";
+import { Box, Stack, Center } from "@chakra-ui/react";
 // import Link from "next/link";
 
 export default function Page() {
-	return (
-		<Box h="100vh" w="100vw" bg="white">
-			<Stack h="100%" w="100%" direction={['column', 'row']} gap={"0"}>
-				<Center h="100%" w="100%">home</Center>
-			</Stack>
-		</Box>
-	)
+  return (
+    <Box h="100vh" w="100vw" bg="white">
+      <Stack h="100%" w="100%" direction={["column", "row"]} gap={"0"}>
+        <Center h="100%" w="100%">
+          home
+        </Center>
+      </Stack>
+    </Box>
+  );
 }
