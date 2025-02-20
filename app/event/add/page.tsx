09a9666e5d39ac7e7f2a08682c4ef129911c48ae
tsukuru296 <tututuku.fufufu296@gmail.com>
2025-02-20 18:21:12 +0900

    プルダウンに変更　inputで入力されたデータを変数に入れる仕組みを追加　「HTML講習会」とかの具体的な値を設定

--- app/event/add/page.tsx
+++ app/event/add/page.tsx
@@ -1,15 +1,48 @@
-"use client"
-import { Box,Button,Center,Input, Stack} from "@chakra-ui/react";
-export default function eventAdd(){
-	return (
-        <Center h="100vh" w="100vw">
-            <Stack w={["80%","50%"]} gap="10">
-                <Box fontSize="30px" textAlign="center">イベントを作成する</Box>
-                <Input placeholder='イベント名' />
-                <Input type="date" />
-                <Input placeholder='公開範囲' />
-                <Button size="lg" colorScheme="purple">作成</Button>
-            </Stack>
-        </Center>
-    )
+"use client";
+import {
+  Box,
+  Button,
+  Center,
+  Input,
+  Stack,
+  Select,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
+export default function eventAdd() {
+  const [eventname, setEventname] = useState("");
+  const [date, setDate] = useState("");
+  const [range, setRange] = useState("");
+  return (
+    <Center>
+      <Stack w={["80%", "50%"]} gap="10">
+        <Box fontSize="30px" textAlign="center">
+          イベントを作成する
+        </Box>
+        <>
+          <Stack>
+            <Text fontSize="15px">イベント名</Text>
+            <Input
+              placeholder="HTML講習会"
+              value={eventname}
+              onChange={(e) => setEventname(e.target.value)}
+            />
+          </Stack>
+        </>
+        <Input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <Select placeholder="公開範囲" value={range} onChange={(e) => setRange(e.target.value)}>
+          <option value="option1">一般公開</option>
+          <option value="option2">非公開</option>
+          <option value="option3">限定公開</option>
+        </Select>
+        <Button size="lg" colorScheme="purple">
+          作成
+        </Button>
+      </Stack>
+    </Center>
+  );
 }
