30e10ad5150ca41cf09ca9a71472fcf26c4b36fa
tsukuru296 <tututuku.fufufu296@gmail.com>
2025-02-21 13:27:56 +0900

    update

--- app/event/add/page.tsx
+++ app/event/add/page.tsx
@@ -12,33 +12,40 @@ import { useState } from "react";
 export default function eventAdd() {
   const [eventname, setEventname] = useState("");
   const [date, setDate] = useState("");
-  const [range, setRange] = useState("");
+  const [range, setRange] = useState("option1");
   return (
     <Center>
-      <Stack w={["80%", "50%"]} gap="10">
+      <Stack  w={["80%", "50%"]} gap="5">
         <Box fontSize="30px" textAlign="center">
           イベントを作成する
         </Box>
-        <>
-          <Stack>
-            <Text fontSize="15px">イベント名</Text>
-            <Input
-              placeholder="HTML講習会"
-              value={eventname}
-              onChange={(e) => setEventname(e.target.value)}
-            />
-          </Stack>
-        </>
-        <Input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <Select placeholder="公開範囲" value={range} onChange={(e) => setRange(e.target.value)}>
-          <option value="option1">一般公開</option>
-          <option value="option2">非公開</option>
-          <option value="option3">限定公開</option>
-        </Select>
+        <Stack>
+          <Text fontSize="15px">イベント名</Text>
+          <Input
+            placeholder="HTML講習会"
+            value={eventname}
+            onChange={(e) => setEventname(e.target.value)}
+          />
+        </Stack>
+        <Stack>
+          <Text fontSize="15px">日時</Text>
+          <Input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </Stack>
+        <Stack>
+          <Text fontSize="15px">公開範囲</Text>
+          <Select
+            placeholder="一般公開"
+            value={range}
+            onChange={(e) => setRange(e.target.value)}
+          >
+            <option value="option2">非公開</option>
+            <option value="option3">限定公開</option>
+          </Select>
+        </Stack>
         <Button size="lg" colorScheme="purple">
           作成
         </Button>
